72e629425e54dc975860fb3a53b4a6a77de9caea
roberto.jiang@educa.madrid.org <robertojiang84@gmail.com>
2025-09-21 16:12:33 +0200

    Se realizaron los siguientes cambios:
    1.- Casi completado la página de login.
    
    2.- He modificado la clase Libro para que acepte una lista de Generos para los libros e idiomas.
    
    3.- He creado la clase servicio AppDbContext, que me sirvirá para conectarme a la base de datos y guardar información en la misma.

--- Biblioteca/Modelo/Libro.cs
+++ Biblioteca/Modelo/Libro.cs
@@ -1,20 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Biblioteca.Modelo
 {
     public class Libro()
     {
+        [Key]
+        public int Id { get; set; }
         public string ISBN { get; set; }
         public string Titulo { get; set; }
         public string Autor { get; set; }
         public string Editorial { get; set; }
         public DateTime FechaPublicacion { get; set; }
-        public List<string>? Genero { get; set; }
+        public List<string>? Genero { get; set; } = new List<string>();
 
-        public List<string>? Idiomas { get; set; }
+        public List<string>? Idiomas { get; set; } = new List<string>();
         public int Ventas { get; set; }
         public decimal Rating { get; set; }
 
         // string porque guardaremos la imagen con una ruta
-        public string Portada { get; set; }  
+        public string Portada { get; set; }
+
     }
 }
 
---
+++ Biblioteca/Servicios/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Biblioteca.Modelo;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Biblioteca.Servicios
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Libro> Libro { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Favoritos> Favoritos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(
+                "server=localhost;database=biblioteca;user=root;password=1234",
+                new MySqlServerVersion(new Version(8, 0, 33))
+            );
+        }
+    }
+}
