0ffb9a7411c7cc9fa15ff38cbd6b6ecd7494efed
roberto.jiang@educa.madrid.org <robertojiang84@gmail.com>
2025-09-21 17:35:52 +0200

    Vista de la página de Login completado.

--- Biblioteca/AppClass/PaginaLogin.xaml.cs
+++ Biblioteca/AppClass/PaginaLogin.xaml.cs
@@ -2,19 +2,49 @@ namespace Biblioteca.AppClass;
 
 public partial class PaginaLogin : ContentPage
 {
-	public PaginaLogin()
+    private int seleccion = 0; // 0 para iniciar sesion, 1 para registro
+
+    public PaginaLogin()
 	{
 		InitializeComponent();
 	}
 
-	private void OnLoginSelected(object sender, EventArgs e)
+    private async void OnLoginSelected(object sender, EventArgs e)
     {
-        // Lógica para manejar el evento de inicio de sesión
+        if (seleccion == 0) return;
+        seleccion = 0;
+
+        await SliderBackground.TranslateTo(-SelectorGrid.Width / 2, 0, 200, Easing.CubicOut);
+        Grid.SetColumn(SliderBackground, 0);
+        SliderBackground.TranslationX = 0;
+
+        LoginTab.TextColor = Colors.Black;
+        RegisterTab.TextColor = Colors.Gray;
+
+        // Cambiar visibilidad y animar
+        LoginForm.IsVisible = true;
+        RegisterForm.Opacity = 0;
+        RegisterForm.IsVisible = false;
+        await LoginForm.FadeTo(1, 250, Easing.CubicIn);
     }
 
-	private void OnRegisterSelected(object sender, EventArgs e)
+    private async void OnRegisterSelected(object sender, EventArgs e)
     {
-        // Lógica para manejar el evento de registro
+        if (seleccion == 1) return;
+        seleccion = 1;
+
+        await SliderBackground.TranslateTo(SelectorGrid.Width / 2, 0, 200, Easing.CubicOut);
+        Grid.SetColumn(SliderBackground, 1);
+        SliderBackground.TranslationX = 0;
+
+        LoginTab.TextColor = Colors.Gray;
+        RegisterTab.TextColor = Colors.Black;
+
+        // Cambiar visibilidad y animar
+        LoginForm.IsVisible = false;
+        RegisterForm.Opacity = 0;
+        RegisterForm.IsVisible = true;
+        await RegisterForm.FadeTo(1, 250, Easing.CubicIn);
     }
 
     private void OnRegisterButtonClicked(object sender, EventArgs e)
